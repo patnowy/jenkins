fb8ad95e25c67a325dcd6886a8ccf78d046a6dea
patnowy <patryk.nowoszynski@gmail.com>
2019-06-22 05:20:47 -0400

    curl check

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -16,6 +16,12 @@ mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
+#curl check
+if ! [ "$(command -v curl)" ]; then
+	echo "Error: curl is not installed."
+	apt-get install -y curl
+fi
+
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
